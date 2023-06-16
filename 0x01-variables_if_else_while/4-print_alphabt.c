13330da0939ae1f8befe02f255ad0c3da9a2763e
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-16 07:24:14 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(void)
+
 {
 char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++) {
-if (letter != 'q' && letter != 'e') {
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
 putchar(letter);
 }
-}
-
-putchar('\n');
-    
+putchar('\n');   
 return 0;
 }
 
