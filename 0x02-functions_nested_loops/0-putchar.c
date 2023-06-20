bcee86154ad3ad59b79388df2a39bb115faa550d
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-20 02:49:44 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry int of my code
+ * return: always 0, success
+ * C- functions, nested loops
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
