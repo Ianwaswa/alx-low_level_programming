bf058f283ea12665987b10735c73ec4605c7fcbe
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-29 04:51:54 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,17 @@
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
+	int i;
 
-	while (*s1 && *s2 && *s1 == *s2)
+	i = 0;
+	while (s1[1] != '\0' && s2[i] != '\0')
 	{
-	s1++;
-	s2++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	return (*s1 - *s2);
+	return (0);
 }
