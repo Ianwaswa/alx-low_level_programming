63663daf5c53c91557384e6997d873683588a7c3
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-29 07:39:33 -0700

    task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @n: integers
+ * @m: integer
+ * Return: always 0
+ */
+
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
+
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+
