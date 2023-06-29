062a3ecdaf41541243f2aa865c2bfbc7d8b77901
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-29 06:40:37 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,6 +14,7 @@ char *rot13(char *s)
 
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTVUWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
