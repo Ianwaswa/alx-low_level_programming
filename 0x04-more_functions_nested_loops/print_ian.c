309bc44bde6ef29efd15802d13c0f03433a86734
Ianwaswa <ianwaswa2019@gmail.com>
2023-07-02 05:17:32 -0700

    task sunday

---
+++ 0x04-more_functions_nested_loops/print_ian.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * ian - function to print
+ * return: 0 always, success
+ */
+int main(void)
+{
+	int n = 18;
+	
+	if n >= 18
+	printf("Eligible to vote");
+	else
+	printf("Not eligible to vote");
+	return(0);
+}
