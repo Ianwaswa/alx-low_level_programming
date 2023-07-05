f2a833f08ca70132c678e71e4b467217ace87582
Ianwaswa <ianwaswa2019@gmail.com>
2023-07-05 02:19:45 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - my function
+ * main - check the code
+ * @str: string
+ * Return: 0 always
+ */
+void _puts_recursion(char str)
+{
+	char str[] = "My string";
+
+	_putchar("%s\n", str);
+
+	return (0);
+}
