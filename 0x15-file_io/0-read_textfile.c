8fa6d09f685e8e97ca2bf283ec01dfdc64cbef11
Ianwaswa <ianwaswa2019@gmail.com>
2023-08-07 03:26:09 -0700

    new changes

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "MAIN_H"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename for the file
+ *
+ * #letters: numbers of letters to print
+ *
+ * Return: numbers of letters printed. Otherwise, print 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+	int filedesc;
+	ssize_t nrd, nwr;
+	char *but;
+
+	if (!filename)
+		return (0);
+	filedesc = open(filename, O_RDONLY);
+
+	if (filedesc == -1)
+		return (0);
+
+	but = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(filedesc, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(filedesc);
+
+	free(buf);
+
+	return (nwr);
+}
