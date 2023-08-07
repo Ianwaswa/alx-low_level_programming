41393b5fe2ab8119f8fa38f24670f27a0dd31d93
Ianwaswa <ianwaswa2019@gmail.com>
2023-08-07 05:23:47 -0700

    new changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,25 +12,26 @@
  */
 
 int append_text_to_file(const char *filename, char *text_content)
-
 {
-	int fd, err, len;
+	int fd1, num_letters, rw;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd < 0) /* failed opening file */
+
+	fd1 = open(filename, O_WRONLY | O_APPEND);
+	if (fd1 == -1)
 		return (-1);
-	if (!text_content)
-	{ /* there is a file, but no new content to write to it */
-		close(fd);
-		return (1);
+
+	if (text_content)
+	{
+		for (num_letters = 0; text_content[num_letters]; ++num_letters)
+			;
+		rw = write(fd1, text_content, num_letters);
+		if (rw == -1)
+			return (-1);
 	}
-	while (*(text_content + len))
-		len++;
-	err = write(fd, text_content, len);
-	close(fd);
-	if (err < 0)
-		return (-1);
+
+	close(fd1);
+
 	return (1);
 }
