dfbe2817ede0e8d12e32dad9dc17e2a09f08a68b
Ianwaswa <ianwaswa2019@gmail.com>
2023-08-07 03:45:16 -0700

    new changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,45 @@
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename for the file
+ * read_textfile - reads a text file and prints the contents to STDOUT
+ * @filename: the filename to open
+ * @letters: number of letters to read and print
  *
- * #letters: numbers of letters to print
- *
- * Return: numbers of letters printed. Otherwise, print 0
+ * Return: the number of letters read and printed, 0 on failure
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
-	int filedesc;
-	ssize_t nrd, nwr;
-	char *but;
+	int fd = -1, err = 0, len = 0;
+	char *temp_b;
 
 	if (!filename)
 		return (0);
-	filedesc = open(filename, O_RDONLY);
-
-	if (filedesc == -1)
+	temp_b = malloc(sizeof(char) * letters);
+	if (!temp_b)
 		return (0);
-
-	but = malloc(sizeof(char) * (letters));
-	if (!buf)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(temp_b);
 		return (0);
-
-	nrd = read(filedesc, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(filedesc);
-
-	free(buf);
-
-	return (nwr);
+	}
+	err = read(fd, temp_b, letters);
+	if (err < 0)
+	{
+		free(temp_b);
+		return (0);
+	}
+	while (temp_b && temp_b[len])
+		len++;
+	err = write(STDOUT_FILENO, temp_b, len);
+	free(temp_b);
+	close(fd);
+	if (err < 0)
+		return (0);
+	return ((ssize_t)err);
 }
