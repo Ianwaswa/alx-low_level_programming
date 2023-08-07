a6730c856c9ff918e7a3d42ea86de2ec67faf079
Ianwaswa <ianwaswa2019@gmail.com>
2023-08-07 06:10:34 -0700

    new changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,21 +1,21 @@
-#include "main.h"
-#include <stdlib.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - entry point for program to get header of ELF file
- * @argc: count of arguments present in CLI
- * @argv: pointer to array of pointers to arguments
- *
- * Return: 1 on success, error code on failure
- */
-
-int main(int argc, char *argv[])
-
-{
-	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
-	return (1);
-}
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
@@ -27,7 +27,7 @@ void check_elf(unsigned char *e_ident)
 {
 	int index;
 
-	for (index = 0; index < 4; index++)
+		for (index = 0; index < 4; index++)
 	{
 		if (e_ident[index] != 127 &&
 		    e_ident[index] != 'E' &&
