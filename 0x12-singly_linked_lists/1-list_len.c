9920325bf9d7a1c65ff3cee85a4186166553c39e
Ianwaswa <ianwaswa2019@gmail.com>
2023-08-14 08:05:46 -0700

    new changes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,7 +6,9 @@
   *
   *Return: Number of elements in a linked list
   */
+
 size_t list_len(const list_t *h)
+
 {
 	int count;
 
@@ -17,5 +19,6 @@ size_t list_len(const list_t *h)
 		h = h->next;
 		count++;
 	}
+	
 	return (count);
 }
