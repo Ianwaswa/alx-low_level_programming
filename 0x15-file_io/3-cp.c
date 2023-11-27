6c078461e86ae05305d0eb16572c4a5375f6211d
Ianwaswa <ianwaswa2019@gmail.com>
2023-11-27 06:03:25 -0800

    new changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,14 @@
-#include "main.h"
+#include "MAIN_H"
+#include <stdio.h>
 
 /**
- * error_file - checks if file can be opened.
- * @file_from: file_from
- * @file_to: file_to
- * @argv: arguments for vector.
- * Return: no return
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-
 void error_file(int file_from, int file_to, char *argv[])
-
 {
 	if (file_from == -1)
 	{
@@ -24,14 +23,12 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for ALX School students.
- * @argc: number of arguments
- * @argv: arguments vector
- * Return: Always 0
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
-
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
@@ -61,14 +58,14 @@ int main(int argc, char *argv[])
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close filedesc %d\n", file_from);
 		exit(100);
 	}
 
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close filedesc %d\n", file_from);
 		exit(100);
 	}
 	return (0);
